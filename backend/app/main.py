6de012a4aa00cbed811059beb6cb388c14ead702
adorda <agustin@pennyworks.com>
2026-01-18 19:12:13 -0300

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://144.33.14.54:3000/", "http://localhost:3000/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
